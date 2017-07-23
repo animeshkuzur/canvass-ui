7f23fc0576ebe9499dbf277b3bd7f78aed4fa03a
Himanshu <himanshu.grover@squareboat.com>
2017-07-23 18:09:11 +0530

    Removed Typeahead plugin

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,8 +2,8 @@
 @include('modals.ask')
 
 <script src="{{ URL::asset('js/vendor/flickity.pkgd.min.js') }}"></script>
-<script src="{{ URL::asset('js/vendor/typeahead.bundle.js') }}"></script>
+<!-- <script src="{{ URL::asset('js/vendor/typeahead.bundle.js') }}"></script> -->
 <script src="{{ URL::asset('js/vendor/select2.min.js') }}"></script>
-<script src="{{ URL::asset('js/typeahead.js') }}"></script>
+<!-- <script src="{{ URL::asset('js/typeahead.js') }}"></script> -->
 <script src="{{ URL::asset('js/main.js')}}"></script>
 
